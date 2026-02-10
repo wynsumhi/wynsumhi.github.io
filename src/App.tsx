50db11256e2ae212e8699bd77fa74c748732a111
wynsumhi <gusdk7244@naver.com>
2026-02-10 21:50:17 +0900

    chore: MUI theme 설정을 ThemeProvider를 통해 앱 전체에 주입

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Pretendard",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "system-ui",
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* MUI의 기본 스타일을 초기화 */}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>portfolio</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
