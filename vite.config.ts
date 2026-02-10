871a5e8d0cae07b7b3cd227481b400942d9f463a
wynsumhi <gusdk7244@naver.com>
2026-02-10 21:07:43 +0900

    chore: vite.config.ts 내 base 경로를 '/'로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 본인의 레파지토리 이름이 'wynsumhi.github.io'라면 '/'를 사용,
+  // 다른 이름(예: 'my-site')이라면 '/my-site/'라고 기재
+  base: '/',
 })
