a0fc6ecbac3493fc72b6c56d35adc378870ff503
gusdk7244 <gusdk7244@naver.com>
2026-02-12 15:55:18 +0900

    feat: 기본 설정 정보 관련 상수 추가

---
+++ src/constants/config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  SITE_TITLE: "HYUNA's Portfolio",
+  SITE_DESCRIPTION: " portfolio website.",
+  GITHUB_URL: "https://github.com/wynsumhi",
+  EMAIL: "gusdk7244@naver.com",
+};
