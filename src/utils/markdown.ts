36e02fa783e3b8138d03c9f6b6c993979a57e98c
gusdk7244 <gusdk7244@naver.com>
2026-02-12 15:51:31 +0900

    feat: 마크다운 관련 유틸리티 추가

---
+++ src/utils/markdown.ts
@@ -0,0 +1,13 @@
+export const extractExcerpt = (
+  markdown: string,
+  length: number = 150,
+): string => {
+  // Markdown 제거하고 본문만 추출
+  const plain = markdown
+    .replace(/#{1,6}\s/g, "") // 헤더 제거
+    .replace(/\*\*(.+?)\*\*/g, "$1") // bold 제거
+    .replace(/\[(.+?)\]\(.+?\)/g, "$1") // 링크 제거
+    .trim();
+
+  return plain.length > length ? plain.substring(0, length) + "..." : plain;
+};
